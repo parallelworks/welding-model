54da87b17d3dc2ed82bc5ed0a80ed8b5ad4ed833
marmar-m <marmar@parallelworks.com>
2018-02-11 21:14:50 -0500

    modify extract.sh and runSalome.sh to run with automatic dockers

--- utils/runSalome.sh
+++ utils/runSalome.sh
@@ -3,7 +3,8 @@ outputDir=$1
 logDir=$2
 weldPassfile=$3
 
-$SALOMEPATH/salome start -t -w 1 utils/Automesh_v14.py  args:--out_dir=$outputDir,--log_dir=$logDir,--weld_pass_coordinates_file=$weldPassfile,--write_separate_step_files
+export PATH=$SALOMEPATH:$PATH						 
+salome start -t -w 1 utils/Automesh_v14.py  args:--out_dir=$outputDir,--log_dir=$logDir,--weld_pass_coordinates_file=$weldPassfile,--write_separate_step_files
 
 cd $outputDir
 tar -cf model_step.tar  model_step?*.inp
