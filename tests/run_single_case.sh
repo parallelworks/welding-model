004b053dc39553a929e452de2242ceb139e102b3
marmar-m <marmar@parallelworks.com>
2018-02-16 11:53:10 -0500

    modify tests/run_single_case.sh to ensure running under docker

--- tests/run_single_case.sh
+++ tests/run_single_case.sh
@@ -12,10 +12,10 @@ mkdir -p results/logFiles/
 ./utils/determine_passes_arc_v4.out  inputs/test_run/eweld.in results/case_1/
 
 # Run Salome to creat the mesh (unv file):
-./utils/runSalome.sh inputs/test_run/eweld.in inputs/test_run/eweld_weld_parameters.in results/case_1/ results/logFiles results/case_1/pass_coordinates.out
+/bin/bash  ./utils/runSalome.sh inputs/test_run/eweld.in inputs/test_run/eweld_weld_parameters.in results/case_1/ results/logFiles results/case_1/pass_coordinates.out
 
 # Convert the unv files to CaluculiX/Abaqus format 
-./utils/runCGX.sh results/case_1/Mesh_3D.unv utils/write_film.fbd results/case_1/model_film.in results/case_1/nodesElems.inp
+/bin/bash ./utils/runCGX.sh results/case_1/Mesh_3D.unv utils/write_film.fbd results/case_1/model_film.in results/case_1/nodesElems.inp
 
 # Create CalculiX analysis files and archive them
 python2 utils/Analysis_file_create.py \
@@ -31,10 +31,10 @@ python2 utils/Analysis_file_create.py --model_inp_file=results/case_1/nodesElems
 tar -cf results/case_1/analFiles.tar -C results/case_1/ model_bc.in model_ini_temperature.in model_material.in 
 
 # Compile CalculiX
-./utils/compileCcx.sh results/case_1/model_dflux.for tools/CalculiX-PW.tar results/case_1/ccx_2.12_MT
+/bin/bash ./utils/compileCcx.sh results/case_1/model_dflux.for tools/CalculiX-PW.tar results/case_1/ccx_2.12_MT
 
 # Run CacluliX
-./utils/runCCX.sh results/case_1/model_step.tar results/case_1/analFiles.tar results/case_1/ccx_2.12_MT results/case_1/pass_coordinates.out results/case_1/model_film.in results/case_1/nodesElems.inp results/case_1/ccx-results.tar 4 
+/bin/bash ./utils/runCCX.sh results/case_1/model_step.tar results/case_1/analFiles.tar results/case_1/ccx_2.12_MT results/case_1/pass_coordinates.out results/case_1/model_film.in results/case_1/nodesElems.inp results/case_1/ccx-results.tar 4 
 
 # Run post-processing
-./utils/mexdex/extract.sh results/case_1/model_step.tar results/case_1/ccx-results.tar setting/mex/welding_anim.json results/case_1/mex/ results/case_1/mex/metrics.csv results/case_1/pass_coordinates.out 
+/bin/bash  ./utils/mexdex/extract.sh results/case_1/model_step.tar results/case_1/ccx-results.tar setting/mex/welding_anim.json results/case_1/mex/ results/case_1/mex/metrics.csv results/case_1/pass_coordinates.out 
